cb2883d38fdff763dd72cffe28897b4364f42263
Armando Rodríguez Vera <jarodriguezvera@gmail.com>
2018-05-03 20:03:06 -0500

    Original

--- BusquedaForm/Form1.Designer.cs
+++ BusquedaForm/Form1.Designer.cs
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richtexBusca = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richtexBusca
+            // 
+            this.richtexBusca.Location = new System.Drawing.Point(24, 35);
+            this.richtexBusca.Name = "richtexBusca";
+            this.richtexBusca.Size = new System.Drawing.Size(371, 252);
+            this.richtexBusca.TabIndex = 0;
+            this.richtexBusca.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(419, 316);
+            this.Controls.Add(this.richtexBusca);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richtexBusca;
     }
 }
 
