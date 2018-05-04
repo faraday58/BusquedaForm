a61607d09f2b3aeb8db318829b2d25e4ddc2ce2a
Armando Rodríguez Vera <jarodriguezvera@gmail.com>
2018-05-03 20:33:28 -0500

    Agregando Librería de busqueda

---
+++ MetodosBusqueda/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MetodosBusqueda
+{
+    public class Busqueda
+    {
+        public int Binaria(int []V,int X)
+        {
+            int N = V.Length;
+            int IZQ = 0;
+            int DER = N;
+            int CEN = (IZQ + DER) / 2;
+            bool BanNoEncuentra = true;
+
+            while( IZQ < DER && BanNoEncuentra)
+            {
+                if( V[CEN] == X)
+                {
+                    BanNoEncuentra = false;
+
+                }
+                else if( V[CEN]> X  )
+                {
+                    IZQ = CEN + 1;
+
+                    CEN = (IZQ + DER) / 2;
+                }
+                else
+                {
+                    DER = CEN;
+                    CEN = (IZQ + DER) / 2;
+
+                }
+
+            }
+            if(BanNoEncuentra)
+            {
+                return -1;
+            }
+            else
+            {
+                return CEN;
+            }
+
+        }
+
+    }
+}
---
+++ MetodosBusqueda/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MetodosBusqueda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MetodosBusqueda")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30006aca-73a2-4357-83b6-3575c7a477c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
